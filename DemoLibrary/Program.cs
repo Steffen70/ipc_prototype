8fa887646833c9c187e5241ea105b08a307c042f
Steffen70 <steffen@seventy.mx>
2025-05-30 11:50:29 +0200

    Initialized IPC prototype with a .NET 8 server and Mono-compatible .NET Framework library project.

---
+++ DemoLibrary/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SwissPension.IpcPrototype.Library
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello from DemoLibrary");
+        }
+    }
+}
